240178cae48597b68bba6403966cda648d104fe8
Cricchio <67266943+Cricchio@users.noreply.github.com>
2023-10-22 22:08:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
-# Import delle librerie
 import streamlit as st
 import httplib2
 import pandas as pd
 from apiclient.discovery import build
 from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.file import Storage
+
+# Funzione per autorizzare l'app e ottenere le credenziali
+def authorize_app(client_id, client_secret, oauth_scope, redirect_uri):
+    # Flusso di autorizzazione OAuth
+    flow = OAuth2WebServerFlow(client_id, client_secret, oauth_scope, redirect_uri)
+    
+    # Verifica se le credenziali sono già memorizzate nella cache
+    storage = Storage("cached_credentials.json")
+    credentials = storage.get()
+    
+    if credentials is None:
+        # Se non ci sono credenziali memorizzate, richiedi l'autorizzazione
+        authorize_url = flow.step1_get_authorize_url(redirect_uri)
+        st.write(f"Per autorizzare l'app, segui [questo link]({authorize_url})")
+        auth_code = st.text_input('Inserisci il tuo Authorization Code qui:')
+        
+        if auth_code:
+            try:
+                # Scambia l'Authorization Code per le credenziali
+                credentials = flow.step2_exchange(auth_code)
+                
+                # Salva le credenziali nella cache
+                storage.put(credentials)
+            except Exception as e:
+                st.write(f"Errore durante l'autorizzazione: {e}")
+    
+    return credentials
 
 # Pagina iniziale
 st.title('Google Search Console Link Suggestions')
@@ -19,61 +46,40 @@ OAUTH_SCOPE = 'https://www.googleapis.com/auth/webmasters.readonly'
 # URI di reindirizzamento
 REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 
-# Variabile per la gestione dell'autorizzazione
-authorized = False
-
 # Seleziona un sito dalla lista
 if CLIENT_ID and CLIENT_SECRET:
-    # Flusso di autorizzazione OAuth
-    flow = OAuth2WebServerFlow(
-        CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, redirect_uri=REDIRECT_URI
-    )
+    # Autorizza l'app e ottieni le credenziali
+    credentials = authorize_app(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
     
-    # Verifica se l'app è già autorizzata
-    if st.button('Autorizza l\'app'):
-        authorize_url = flow.step1_get_authorize_url()
-        st.write(f"Per autorizzare l'app, segui [questo link]({authorize_url})")
-    auth_code = st.text_input('Inserisci il tuo Authorization Code qui:')
-    
-    # Se l'Authorization Code è stato inserito
-    if auth_code:
-        try:
-            # Scambia l'Authorization Code per le credenziali
-            credentials = flow.step2_exchange(auth_code)
-            http = httplib2.Http()
-            creds = credentials.authorize(http)
-            webmasters_service = build('searchconsole', 'v1', http=creds)
-            authorized = True
-        except Exception as e:
-            st.write(f"Errore durante l'autorizzazione: {e}")
-
-    if authorized:
+    if credentials:
+        # Crea un oggetto http autorizzato
+        http = credentials.authorize(httplib2.Http())
+        
+        # Crea il servizio Google Search Console
+        webmasters_service = build('searchconsole', 'v1', http=http)
+        
         # Ottieni la lista dei siti nell'account Google Search Console
         site_list = webmasters_service.sites().list().execute()
-
+        
         # Seleziona un sito dalla lista
         selected_site = st.selectbox('Seleziona un sito web:', [site['siteUrl'] for site in site_list['siteEntry']])
-
-        # Se un sito è stato selezionato
-        if selected_site:
-            st.write(f'Hai selezionato il sito web: {selected_site}')
-
-            # Inserisci l'URL da ispezionare
-            url_to_inspect = st.text_input('Inserisci l\'URL da ispezionare:')
-
-            # Esegui l'ispezione
-            if st.button('Ispeziona URL'):
-                request_body = {
-                    'inspectionUrl': url_to_inspect,
-                    'siteUrl': selected_site
-                }
-                response = webmasters_service.urlInspection().index().inspect(body=request_body).execute()
-                st.write(f'Risultato dell\'ispezione: {response}')
+        
+        # Inserisci l'URL da ispezionare
+        url_to_inspect = st.text_input('Inserisci l\'URL da ispezionare:')
+        
+        # Esegui l'ispezione
+        if st.button('Ispeziona URL'):
+            request_body = {
+                'inspectionUrl': url_to_inspect,
+                'siteUrl': selected_site
+            }
+            response = webmasters_service.urlInspection().index().inspect(body=request_body).execute()
+            st.write(f'Risultato dell\'ispezione: {response}')
 
                 # Ottieni dati dalla Search Console
-               # start_date = st.date_input('Data di inizio', pd.to_datetime('2023-01-01'))
-                #end_date = st.date_input('Data di fine', pd.to_datetime('2023-10-28'))
-                #row_limit = st.number_input('Limite di righe', min_value=1, max_value=25000, value=25000)
+                start_date = st.date_input('Data di inizio', pd.to_datetime('2023-01-01'))
+                end_date = st.date_input('Data di fine', pd.to_datetime('2023-10-28'))
+                row_limit = st.number_input('Limite di righe', min_value=1, max_value=25000, value=25000)
 
                 if st.button('Ottieni dati'):
                     request_body = {
