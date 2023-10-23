4b957aa412d2b0498fe0ca382ead2335123590a9
Cricchio <67266943+Cricchio@users.noreply.github.com>
2023-10-24 00:09:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from langchain.agents import AgentType
-from langchain.agents import create_pandas_dataframe_agent
-from langchain.callbacks import StreamlitCallbackHandler
-from langchain.chat_models import ChatOpenAI
 import streamlit as st
 import httplib2
 import pandas as pd
@@ -55,6 +51,7 @@ def authorize_app(client_id, client_secret, oauth_scope, redirect_uri):
 st.title('Google Search Console API Companion')
 st.write("Google Cloud Console: https://console.cloud.google.com/apis/credentials")
 
+
 # Inserimento delle credenziali
 st.subheader('Inserisci le tue credenziali Google Cloud Project:')
 CLIENT_ID = st.text_input('Client ID')
@@ -80,92 +77,56 @@ if CLIENT_ID and CLIENT_SECRET:
         # Seleziona un sito dalla lista
         st.session_state.selected_site = st.selectbox('Seleziona un sito web:', st.session_state.available_sites)
 
-# Creazione dei tabs
-tab1, tab2 = st.tabs(["Ispezione URL", "Analytics"])
+        tab1, tab2 = st.tabs(["Ispezione URL", "Analytics"])
+        
+        with tab1:
+            # Inserisci l'URL da ispezionare
+            url_to_inspect = st.text_input("Inserisci l'URL da ispezionare:")
+            
+            # Esegui l'ispezione
+            if st.button('Ispeziona URL'):
+                if st.session_state.selected_site is not None:
+                    request_body = {
+                        'inspectionUrl': url_to_inspect,
+                        'siteUrl': st.session_state.selected_site
+                    }
+                    response = webmasters_service.urlInspection().index().inspect(body=request_body).execute()
+                    st.write(f'Risultato dell\'ispezione: {response}')
 
-if tab1:
-    # Tab "Ispezione URL"
-    # Inserisci l'URL da ispezionare
-    url_to_inspect = st.text_input("Inserisci l'URL da ispezionare:")
+        with tab2:
+            # Ottieni dati dalla Search Console
+            start_date = st.date_input('Data di inizio', pd.to_datetime('2023-01-01'))
+            end_date = st.date_input('Data di fine', pd.to_datetime('2023-10-28'))
+            row_limit = st.number_input('Limite di righe', min_value=1, max_value=25000, value=25000)
     
-    # Esegui l'ispezione
-    if st.button('Ispeziona URL'):
-        if st.session_state.selected_site is not None:
-            request_body = {
-                'inspectionUrl': url_to_inspect,
-                'siteUrl': st.session_state.selected_site
-            }
-            response = webmasters_service.urlInspection().index().inspect(body=request_body).execute()
-            st.write(f'Risultato dell\'ispezione: {response}')
-
-if tab2:
-    # Tab "Analytics"
-    # Ottieni dati dalla Search Console
-    start_date = st.date_input('Data di inizio', pd.to_datetime('2023-01-01'))
-    end_date = st.date_input('Data di fine', pd.to_datetime('2023-10-28'))
-    row_limit = st.number_input('Limite di righe', min_value=1, max_value=25000, value=25000)
-
-    if st.button('Ottieni dati'):
-        if st.session_state.selected_site is not None:
-            request_body = {
-                "startDate": start_date.strftime('%Y-%m-%d'),
-                "endDate": end_date.strftime('%Y-%m-%d'),
-                "dimensions": ['QUERY', 'PAGE'],
-                "rowLimit": row_limit,
-                "dataState": "final"
-            }
-
-            response_data = webmasters_service.searchanalytics().query(siteUrl=st.session_state.selected_site, body=request_body).execute()
-
-            data_list = []
-            for row in response_data['rows']:
-                data_list.append({
-                    'query': row['keys'][0],
-                    'page': row['keys'][1],
-                    'clicks': row['clicks'],
-                    'impressions': row['impressions'],
-                    'ctr': row['ctr'],
-                    'position': row['position']
-                })
-
-            df = pd.DataFrame(data_list)
-            st.dataframe(df)
-            #questa parte che segue è quella AI
-
-openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
-
-if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
-
-if st.button("Send"):
-    user_input = st.text_area("You: ", placeholder="What is this data about?")
-    if user_input:
-        st.session_state.messages.append({"role": "user", "content": user_input})
-
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
-
-    llm = ChatOpenAI(
-        temperature=0,
-        model="gpt-3.5-turbo-0613",
-        openai_api_key=openai_api_key,
-        streaming=True
-    )
-
-    pandas_df_agent = create_pandas_dataframe_agent(
-        llm,
-        df,
-        verbose=True,
-        agent_type=AgentType.OPENAI_FUNCTIONS,
-        handle_parsing_errors=True,
-    )
-
-    with st.chat_message("assistant"):
-        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-        response = pandas_df_agent.run(st.session_state.messages, callbacks=[st_cb])
-        st.session_state.messages.append({"role": "assistant", "content": response})
-        st.write(response)
+            if st.button('Ottieni dati'):
+                if st.session_state.selected_site is not None:
+                    request_body = {
+                        "startDate": start_date.strftime('%Y-%m-%d'),
+                        "endDate": end_date.strftime('%Y-%m-%d'),
+                        "dimensions": ['QUERY', 'PAGE'],
+                        "rowLimit": row_limit,
+                        "dataState": "final"
+                    }
+    
+                    response_data = webmasters_service.searchanalytics().query(siteUrl=st.session_state.selected_site, body=request_body).execute()
+    
+                    data_list = []
+                    for row in response_data['rows']:
+                        data_list.append({
+                            'query': row['keys'][0],
+                            'page': row['keys'][1],
+                            'clicks': row['clicks'],
+                            'impressions': row['impressions'],
+                            'ctr': row['ctr'],
+                            'position': row['position']
+                        })
+    
+                    df = pd.DataFrame(data_list)
+                    st.dataframe(df)
+                    
+    
+                    # Filtra e suggerisci pagine interne
+                    #filtered_data = df[(df['position'] >= 11) & (df['position'] <= 20) & (df['impressions'] >= 100)]
+                    #st.subheader('Suggerimenti di pagine interne:')
+                    #st.dataframe(filtered_data)
