b46281b0855b7a3b9f82e594bb1c02c12ccd84bb
Cricchio <67266943+Cricchio@users.noreply.github.com>
2023-10-23 23:40:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ if CLIENT_ID and CLIENT_SECRET:
         with tab2:
             # Ottieni dati dalla Search Console
             start_date = st.date_input('Data di inizio', pd.to_datetime('2023-01-01'))
-	    end_date = st.date_input('Data di fine', pd.to_datetime('2023-10-28'))
+            end_date = st.date_input('Data di fine', pd.to_datetime('2023-10-28'))
             row_limit = st.number_input('Limite di righe', min_value=1, max_value=25000, value=25000)
     
             if st.button('Ottieni dati'):
