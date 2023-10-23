039feca1024aea9b4e3f8e0c131d51647108697f
Cricchio <67266943+Cricchio@users.noreply.github.com>
2023-10-23 21:58:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,21 +17,21 @@ REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 # Imposta lo scope OAuth2
 OAUTH_SCOPE = 'https://www.googleapis.com/auth/webmasters.readonly'
 
-# Creazione del flusso OAuth2
-flow = OAuth2WebServerFlow(client_id, client_secret, OAUTH_SCOPE, REDIRECT_URI)
-
-# Genera l'URL per l'autorizzazione
-authorize_url = flow.step1_get_authorize_url(REDIRECT_URI)
-
-# Pagina di autorizzazione
-st.write("Vai al seguente link nel tuo browser:")
-st.write(authorize_url)
-
-# Ottieni l'autorizzazione dall'utente
-auth_code = st.text_input("Inserisci il codice di autorizzazione qui:")
-
-# Scambio del codice di autorizzazione con le credenziali
 if st.button("Genera Credenziali"):
+    # Creazione del flusso OAuth2
+    flow = OAuth2WebServerFlow(client_id, client_secret, OAUTH_SCOPE, REDIRECT_URI)
+
+    # Genera l'URL per l'autorizzazione
+    authorize_url = flow.step1_get_authorize_url(REDIRECT_URI)
+
+    # Pagina di autorizzazione
+    st.write("Vai al seguente link nel tuo browser:")
+    st.write(authorize_url)
+
+    # Ottieni l'autorizzazione dall'utente
+    auth_code = st.text_input("Inserisci il codice di autorizzazione qui:")
+
+    # Scambio del codice di autorizzazione con le credenziali
     credentials = flow.step2_exchange(auth_code)
     st.write("Credenziali generate con successo!")
 
