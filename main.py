bf3712e338c7719d8d1d95cc362a2741b52a7da4
Cricchio <67266943+Cricchio@users.noreply.github.com>
2023-10-23 23:46:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from langchain.agents import AgentType
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.chat_models import ChatOpenAI
 import streamlit as st
 import httplib2
 import pandas as pd
@@ -124,6 +128,39 @@ if CLIENT_ID and CLIENT_SECRET:
     
                     df = pd.DataFrame(data_list)
                     st.dataframe(df)
+                    
+                    openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+                    if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
+                        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+                    
+                    for msg in st.session_state.messages:
+                        st.chat_message(msg["role"]).write(msg["content"])
+                    
+                    if prompt := st.chat_input(placeholder="What is this data about?"):
+                        st.session_state.messages.append({"role": "user", "content": prompt})
+                        st.chat_message("user").write(prompt)
+                    
+                        if not openai_api_key:
+                            st.info("Please add your OpenAI API key to continue.")
+                            st.stop()
+                    
+                        llm = ChatOpenAI(
+                            temperature=0, model="gpt-3.5-turbo-0613", openai_api_key=openai_api_key, streaming=True
+                        )
+                    
+                        pandas_df_agent = create_pandas_dataframe_agent(
+                            llm,
+                            df,
+                            verbose=True,
+                            agent_type=AgentType.OPENAI_FUNCTIONS,
+                            handle_parsing_errors=True,
+                        )
+                    
+                    with st.chat_message("assistant"):
+                            st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+                            response = pandas_df_agent.run(st.session_state.messages, callbacks=[st_cb])
+                            st.session_state.messages.append({"role": "assistant", "content": response})
+                            st.write(response)
                     
     
                     # Filtra e suggerisci pagine interne
