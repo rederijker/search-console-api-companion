41a622c3ef2077d2390acd6a83f6704c6aa96393
Cricchio <67266943+Cricchio@users.noreply.github.com>
2023-10-26 23:15:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ if CLIENT_ID and CLIENT_SECRET:
                 check_box_row = st.radio(row_limit_options)
                 if check_box_row == 'Yes':
                     row_limit = st.number_input('Row limit', min_value=1, max_value=25000, value=25000)
-                else
+                
             with col4:
                 # Opzioni per il tipo di dati nell'API
                 options_type = {
