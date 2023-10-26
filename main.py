192673ac11e8de937f7700ba452b47213aaca52c
Cricchio <67266943+Cricchio@users.noreply.github.com>
2023-10-26 22:24:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -146,8 +146,8 @@ if CLIENT_ID and CLIENT_SECRET:
     
                     df = pd.DataFrame(data_list)
                     st.dataframe(df)
-                    chart_data = df(np.random.randn(20, 3), columns=["impressions","date"])
-                    st.line_chart(chart_data, x="date", y=["impressions"], color=["#FF0000"])
+                    chart_data = df(columns=["impressions","date"])
+                    st.line_chart(chart_data, x="impressions", y=["date"], color=["#FF0000"])
                  
 
                     
