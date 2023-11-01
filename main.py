8786074858615152cc4e46d738915669f1fa32f9
Cristiano Caggiula <67266943+Cricchio@users.noreply.github.com>
2023-11-01 14:17:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go  # Importa il modulo go da Plotly
 import time
+from streamlit_extras.metric_cards import style_metric_cards 
 
 st.set_page_config(
     page_title="Search Console API Companion",
