d03673468fbec898f98e3c11c1cc22d9ec4284a1
Cristiano Caggiula <67266943+Cricchio@users.noreply.github.com>
2023-11-01 21:37:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -339,42 +339,43 @@ if CLIENT_ID and CLIENT_SECRET:
                             df_lower_low_ctr = lower_low_ctr[['Query', 'Page', 'Impressions', 'Clicks', 'CTR', 'Position']]
                             df_upper_low_ctr = upper_low_ctr[['Query', 'Page', 'Impressions', 'Clicks', 'CTR', 'Position']]
                             #mostrare df
+                     
+
+
+                            with st.expander("Top position and high CTR Queries"):           
+                                st.write("For these queries, there's not much you need to do; you're already doing a great job.")
+                                st.write(df_upper_high_ctr)
+                            with st.expander("Low position and high CTR Queries"):
+                                st.write("""
+                                Those queries appear to be highly relevant to users. They achieve a high click-through rate (CTR) even when they rank lower than the average query on your website. If the average position of these queries improves, it could significantly impact your website's performance. It's advisable to focus on enhancing the SEO for these queries. For instance, consider a prominent query in quadrant 2 for a gardening website, such as "how to build a wooden shed." Check if you already have a dedicated page for this topic and proceed in two ways:
+    
+                                -If you don't have a dedicated page, think about creating one to consolidate all the information on your website related to this subject.
+    
+                                -If you already have a page, contemplate adding more content to better address the needs of users searching for this query.
+                                """)
+                                st.write(df_lower_high_ctr)
+                            with st.expander("Low position and low CTR Queries"):
+                                st.write("""
+                                When looking at queries with low CTR (both with low and top position), it's especially interesting to look at the bubble sizes to understand which queries have a low CTR but are still driving significant traffic. While the queries in this quadrant might seem unworthy of your effort, they can be divided into two main groups:
+                                
+                                **Related queries**: If the query in question is important to you, it's a good start to have it appearing in Search already. Prioritize these queries over queries that are not appearing in Search results at all, as they'll be easier to optimize.
+                                
+                                **Unrelated queries**: If your site doesn't cover content related to this query, maybe it's a good opportunity to fine tune your content or focus on queries that will bring relevant traffic.
+                                """)
+                                st.write(df_lower_low_ctr)
+                            with st.expander("Top position and low CTR Queries"):
+                                st.write("""
+                                These queries might have a low click-through rate (CTR) for various reasons. Check the largest bubbles to find signs of the following:
+    
+                                Your competitors may be using structured data markup and appearing with rich results, attracting users to click on their results instead of yours. Consider optimizing for the most common visual elements in Google Search.
+    
+                                You may have optimized, or be "accidentally" ranking for a query that users are not interested in relation to your site. This might not be an issue for you, in which case you can ignore those queries. If you prefer people not to find you through those queries (for example, they contain offensive words), try to fine-tune your content to remove mentions that could be seen as synonyms or related queries to the one bringing traffic.
+    
+                                People may have already found the information they needed, for example, your company's opening hours, address, or phone number. Check the queries that were used and the URLs that contained the information. If one of your website goals is to drive people to your stores, this is working as intended; if you believe that people should visit your website for extra information, you could try to optimize your titles and descriptions to make that clear. See the next section for more details.
+                                """)
+                                st.write(df_upper_low_ctr)
                         except KeyError as e:
                             st.text("Il campo 'Page' non è presente nel DataFrame df_upper_high_ctr")
-
-
-                        with st.expander("Top position and high CTR Queries"):           
-                            st.write("For these queries, there's not much you need to do; you're already doing a great job.")
-                            st.write(df_upper_high_ctr)
-                        with st.expander("Low position and high CTR Queries"):
-                            st.write("""
-                            Those queries appear to be highly relevant to users. They achieve a high click-through rate (CTR) even when they rank lower than the average query on your website. If the average position of these queries improves, it could significantly impact your website's performance. It's advisable to focus on enhancing the SEO for these queries. For instance, consider a prominent query in quadrant 2 for a gardening website, such as "how to build a wooden shed." Check if you already have a dedicated page for this topic and proceed in two ways:
-
-                            -If you don't have a dedicated page, think about creating one to consolidate all the information on your website related to this subject.
-
-                            -If you already have a page, contemplate adding more content to better address the needs of users searching for this query.
-                            """)
-                            st.write(df_lower_high_ctr)
-                        with st.expander("Low position and low CTR Queries"):
-                            st.write("""
-                            When looking at queries with low CTR (both with low and top position), it's especially interesting to look at the bubble sizes to understand which queries have a low CTR but are still driving significant traffic. While the queries in this quadrant might seem unworthy of your effort, they can be divided into two main groups:
-                            
-                            **Related queries**: If the query in question is important to you, it's a good start to have it appearing in Search already. Prioritize these queries over queries that are not appearing in Search results at all, as they'll be easier to optimize.
-                            
-                            **Unrelated queries**: If your site doesn't cover content related to this query, maybe it's a good opportunity to fine tune your content or focus on queries that will bring relevant traffic.
-                            """)
-                            st.write(df_lower_low_ctr)
-                        with st.expander("Top position and low CTR Queries"):
-                            st.write("""
-                            These queries might have a low click-through rate (CTR) for various reasons. Check the largest bubbles to find signs of the following:
-
-                            Your competitors may be using structured data markup and appearing with rich results, attracting users to click on their results instead of yours. Consider optimizing for the most common visual elements in Google Search.
-
-                            You may have optimized, or be "accidentally" ranking for a query that users are not interested in relation to your site. This might not be an issue for you, in which case you can ignore those queries. If you prefer people not to find you through those queries (for example, they contain offensive words), try to fine-tune your content to remove mentions that could be seen as synonyms or related queries to the one bringing traffic.
-
-                            People may have already found the information they needed, for example, your company's opening hours, address, or phone number. Check the queries that were used and the URLs that contained the information. If one of your website goals is to drive people to your stores, this is working as intended; if you believe that people should visit your website for extra information, you could try to optimize your titles and descriptions to make that clear. See the next section for more details.
-                            """)
-                            st.write(df_upper_low_ctr)
 
                     with tab2:
                         st.text("")
