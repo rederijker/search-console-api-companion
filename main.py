7975f4ee4a9b1aadad4688dccaaaaabe4a048aed
Cristiano Caggiula <67266943+Cricchio@users.noreply.github.com>
2023-11-01 21:47:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -313,7 +313,7 @@ if CLIENT_ID and CLIENT_SECRET:
                         fig = px.scatter(df, x='CTR', y='Position', size='Clicks', hover_data=['Query'], log_y=True, log_x=True,)
                         fig.update_yaxes(autorange="reversed")
                         fig.update_xaxes(type="log", tickvals=[0, 0.5, 1], ticktext=["0", "0.5", "1"])
-                        fig.update_yaxes(type="log", tickvals=[1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100], ticktext=["1", "10", "20", "30", "40", "50", "60", "70", "80", "90", 1"00"])
+                        fig.update_yaxes(type="log", tickvals=[1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100], ticktext=["1", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"])
                         
     
                         
