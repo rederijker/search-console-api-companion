e37dccf9e986daaa99fd7d20efd572a640b3d3c4
Cristiano Caggiula <67266943+Cricchio@users.noreply.github.com>
2023-11-01 21:53:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -308,19 +308,22 @@ if CLIENT_ID and CLIENT_SECRET:
                         # Calcola la media per la posizione media e il CTR
                         df = pd.DataFrame(data_list)
                          # Valore minimo
-                        df['CTR'] = df['CTR'] 
+                        # Calcola i valori minimi e massimi per gli assi
+                        min_ctr = df['CTR'].min()
+                        max_ctr = df['CTR'].max()
+                        min_position = df['Position'].min()
+                        max_position = df['Position'].max()
+                        
                         # Crea il grafico a bolle con Plotly
                         fig = px.scatter(df, x='CTR', y='Position', size='Clicks', hover_data=['Query'], log_y=True, log_x=True,)
                         fig.update_yaxes(autorange="reversed")
-                        fig.update_xaxes(type="log", tickvals=[0, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], ticktext=["0", "0.2", "0.3", "0.42", "0.5", "0.6", "0.7", "0.8", "0.9", "1"])
-                        fig.update_yaxes(type="log", tickvals=[1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100], ticktext=["1", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"])
-                        
-    
+                        fig.update_xaxes(type="log", tickvals=[min_ctr, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], ticktext=[str(min_ctr), "0.2", "0.3", "0.42", "0.5", "0.6", "0.7", "0.8", "0.9", "1"])
+                        fig.update_yaxes(type="log", tickvals=[min_position, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100], ticktext=[str(min_position), "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"])
                         
                         # Aggiungi linee di riferimento per la media di CTR e posizione
-                        fig.add_shape(type='line', x0=df['CTR'].mean(), x1=df['CTR'].mean(), y0=df['Position'].min(), y1=df['Position'].max(), line=dict(color='red', dash='dash'))
-                        fig.add_shape(type='line', x0=df['CTR'].min(), x1=df['CTR'].max(), y0=df['Position'].mean(), y1=df['Position'].mean(), line=dict(color='red', dash='dash'))                   
-                     
+                        fig.add_shape(type='line', x0=df['CTR'].mean(), x1=df['CTR'].mean(), y0=min_position, y1=max_position, line=dict(color='red', dash='dash'))
+                        fig.add_shape(type='line', x0=min_ctr, x1=max_ctr, y0=df['Position'].mean(), y1=df['Position'].mean(), line=dict(color='red', dash='dash'))
+
                                     
                         # Mostra il grafico interattivo
                         with st.container():
