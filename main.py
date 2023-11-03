77a5a24afbb3457595c312e7504f428b144f3e34
Cristiano Caggiula <67266943+Cricchio@users.noreply.github.com>
2023-11-03 21:40:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -411,10 +411,14 @@ if CLIENT_ID and CLIENT_SECRET:
                             
                             # Calcola la media dei clic solo tra le pagine distinte
                             average_clic_df_popular = df_popular_page_1['Clicks'].mean()
+                            # Calcola impression solo tra le pagine distinte
+
+                            average_impression_df_popular = df_popular_page_1['Impressions'].mean()
+                            
                             st.write("Media dei clic per le pagine popolari:", average_clic_df_popular)
 
                             # Filtra le pagine con clic maggiori o uguali alla media
-                            popular_pages = df_popular_page_1[df_popular_page_1['Clicks'] > average_clic_df_popular]
+                            popular_pages = df_popular_page_1[(df_popular_page_1['Clicks'] > average_clic_df_popular) & (df_popular_page_1['Impressions'] > average_impression_df_popular)]
                             
                             # Calcola la nuova media solo per le pagine popolari
                             
