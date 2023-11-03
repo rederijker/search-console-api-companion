2dbc2e41d442566bc2c6938d5bf8501a3aaa438d
Cristiano Caggiula <67266943+Cricchio@users.noreply.github.com>
2023-11-03 23:31:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -186,7 +186,7 @@ if CLIENT_ID and CLIENT_SECRET:
                 
         
             # Aggiungi un bottone per ottenere i dati in batch
-            if st.button('GET DATA'):
+            if st.button('⬇️ GET DATA'):
                 if st.session_state.selected_site is not None:
                     start_row = 0  # Inizia dalla prima riga
                     data_list = []  # Inizializza una lista per i dati
