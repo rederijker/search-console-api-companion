b7f1fa714c971543130366fa2f64f7e17a857534
Cristiano Caggiula <67266943+Cricchio@users.noreply.github.com>
2023-11-04 19:49:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -614,7 +614,7 @@ if CLIENT_ID and CLIENT_SECRET:
                                 }
                                 
                                 st_echarts(option=options, theme='chalk', height=400, width='100%')
-                            st.container():
+                            with st.container():
                                 criar_grafico_echarts(df_grouped)
                         else:
                             st.write("tab")
