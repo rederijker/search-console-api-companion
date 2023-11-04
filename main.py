156fd87811653a6017133410f8cd74cbf277b2ea
Cristiano Caggiula <67266943+Cricchio@users.noreply.github.com>
2023-11-04 19:53:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -306,7 +306,7 @@ if CLIENT_ID and CLIENT_SECRET:
                 
 
                     
-                    tab1, tab2, tab3 = st.tabs(["QUERY PERFORMANCE", "PAGE PERFORMANCE", "TRAFFIC REPORT"])
+                    tab1, tab2, tab3 = st.tabs(["QUERY PERFORMANCE", "PAGE PERFORMANCE"])
                     with tab1:
                         if all(dim in selected_dimensions for dim in ['Query', 'Page']):
 
@@ -519,9 +519,9 @@ if CLIENT_ID and CLIENT_SECRET:
                         except KeyError as e:
                             st.warning(e)
                         
-                    with tab3:
-                        if all(dim in selected_dimensions for dim in ['Date', 'Page']):
-                            st.text("")
+                    #with tab3:
+                        #if all(dim in selected_dimensions for dim in ['Date', 'Page']):
+                            #st.text("")
                             
                             
                            
@@ -616,5 +616,5 @@ if CLIENT_ID and CLIENT_SECRET:
                                 st_echarts(option=options, theme='chalk', height=400, width='100%')
                             with st.container():
                                 criar_grafico_echarts(df_grouped)
-                        else:
-                            st.write("tab")
+                        #else:
+                            #st.write("tab")
