a615048b2f2c7dc6cce780456f8fdbbf2573208a
Cristiano Caggiula <67266943+Cricchio@users.noreply.github.com>
2023-11-04 23:00:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -538,7 +538,9 @@ if CLIENT_ID and CLIENT_SECRET:
                     def traffic_report(df_graf):
                         # Formattazione della colonna 'CTR' del DataFrame
                         #df['CTR'] = df_graf['CTR'].apply(lambda ctr: f"{ctr * 100:.2f}")
-                        df['CTR'] = df_graf['CTR'].apply(lambda ctr: f"{ctr * 100:.2f}".replace('.', ','))
+                        #df['CTR'] = df_graf['CTR'].apply(lambda ctr: f"{ctr * 100:.2f}".replace('.', ','))
+                        df['CTR'] = (df_graf['CTR'] * 100).apply('{:.2f}'.format).str.replace('.', ',')
+
 
                         df['Position'] = df_graf['Position'].apply(lambda pos: round(pos, 2))
                     
