49d673e03a8486b5eba10ec3c739e23ecaf2a512
Cristiano Caggiula <67266943+Cricchio@users.noreply.github.com>
2023-11-04 01:06:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -412,16 +412,16 @@ if CLIENT_ID and CLIENT_SECRET:
                             }
                             #Raggruppiamo df
                             df_aggregated_popular_page = df.groupby('Page').agg(agg_funcs).reset_index()
+
                             #Calcoliamo il CTR dividendo click per impression
                             df_aggregated_popular_page['CTR'] = (df_aggregated_popular_page['Clicks'] / df_aggregated_popular_page['Impressions'])
                             # calcoliamo ctr medio e presentiamo il CTR come %
-                            average_ctr_popular = df_aggregated_popular_page['CTR'].mean()
 
                             df_aggregated_popular_page['CTR'] = df_aggregated_popular_page['CTR'].map('{:.2%}'.format)
                             #Cambiano nome alle colonne
                             df_aggregated_popular_page = df_aggregated_popular_page.rename(columns={'CTR': 'Average CTR'})
                             #Formatta la posizione media
-                            
+                            average_ctr_popular = df_aggregated_popular_page['CTR'].mean()
                             df_aggregated_popular_page['Position'] = df_aggregated_popular_page['Position'].round(2)
                             average_position_popular = df_aggregated_popular_page['Position'].mean()
                             #cambia nome alla colonna
@@ -432,7 +432,7 @@ if CLIENT_ID and CLIENT_SECRET:
                             average_impression_df_pupular = df_aggregated_popular_page['Impressions'].mean()
                             # Calcola impression solo tra le pagine distinte                            
                             # Filtra le pagine con clic maggiori o uguali alla media
-                            popular_pages = df_aggregated_popular_page[df_aggregated_popular_page['CTR'] > average_ctr_popular]
+                            popular_pages = df_aggregated_popular_page[df_aggregated_popular_page['Average CTR'] > 1]
                             #ordiniamo per clicks
                             popular_pages = popular_pages.sort_values(by='Clicks', ascending=False)
 
