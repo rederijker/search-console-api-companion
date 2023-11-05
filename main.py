d69ac1c47edd4b8d39b680ddef90fcfb2f66e5ce
Cristiano Caggiula <67266943+Cricchio@users.noreply.github.com>
2023-11-05 20:58:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -426,8 +426,7 @@ if CLIENT_ID and CLIENT_SECRET:
                             fig = px.scatter(df, x='CTR', y='Position', size='Clicks', hover_data=['Query'])
                             
                             fig.update_yaxes(autorange="reversed")
-                            fig.update_yaxes(range=[min_position, max_position])
-                            fig.update_xaxes(range=[min_ctr * 100, max_ctr * 100])
+                    
                             fig.update_xaxes(autorange=True)  # Autoscaling per l'asse X
                           
 
