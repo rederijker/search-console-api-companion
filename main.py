90a588617e49bb3daed1ccac0e9b4a4ec82f31b8
Cristiano Caggiula <67266943+Cricchio@users.noreply.github.com>
2023-11-09 20:48:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -405,34 +405,7 @@ if CLIENT_ID and CLIENT_SECRET:
                     tab1, tab2, tab3 = st.tabs(["QUERY PERFORMANCE", "PAGE PERFORMANCE", "CONTENT OPTIMIZER"])
                     with tab1:
                         if all(dim in selected_dimensions for dim in ['Query', 'Page']):
-                            df = pd.DataFrame(data_list)
-                            min_ctr = df['CTR'].min()
-                            max_ctr = df['CTR'].max()
-                            min_position = df['Position'].min()
-                            max_position = df['Position'].max()
-                            average_ctr = df['CTR'].mean()
-                            average_position = df['Position'].mean()
-                            fig = px.scatter(df, x='CTR', y='Position', size='Clicks', hover_data=['Query'])
-                            for i, row in df.iterrows():
-                                if row['Clicks'] == 0:
-                                    fig.add_trace(go.Scatter(
-                                        x=[row['CTR']],
-                                        y=[row['Position']],
-                                        mode='markers',
-                                        marker=dict(size=row['Clicks'], color='red'),
-                                        hoverinfo='text',
-                                        hovertext=row['Query']
-                      			        ))
-                            fig.update_yaxes(autorange="reversed")
-                            fig.update_yaxes(range=[min_position, max_position])
-                            fig.update_xaxes(range=[min_ctr * 100, max_ctr * 100])
-                            fig.update_xaxes(autorange=True)  # Autoscaling per l'asse X
-                            fig.add_shape(type='line', x0=average_ctr, x1=average_ctr, y0=min_position, y1=max_position, line=dict(color='green', dash='dash'))
-                            fig.add_shape(type='line', x0=min_ctr, x1=max_ctr, y0=average_position, y1=average_position, line=dict(color='green', dash='dash'))
-                                # Mostra il grafico interattivo
-                            st.subheader("Bubble Charts")
-                            st.plotly_chart(fig, use_container_width=True)     
-                                            
+                           
 
 			
                                 
